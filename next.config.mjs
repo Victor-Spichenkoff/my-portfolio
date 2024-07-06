6806553c892df74392f5b6ac6901329c18c463af
Victor-Spichenkoff <victorspich13@gmail.com>
2024-07-06 20:48:25 -0300

    Fix config error  on gh

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 import createNextIntlPlugin from 'next-intl/plugin';
  
-const withNextIntl = createNextIntlPlugin();
+// const withNextIntl = createNextIntlPlugin()
+let withNextIntl
+try {
+    withNextIntl = createNextIntlPlugin();
+  } catch (error) {
+    console.error('Erro ao criar o plugin next-intl:', error)
+    throw error  // Erro de tipo no gh-pages
+  }
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
